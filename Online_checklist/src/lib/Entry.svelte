8087da5961b765b50f319a5df4ae3f4e308c8939
lenni migios <lenwood081@gmail.com>
2024-10-15 18:43:42 +1100

    made basjc project

--- Online_checklist/src/lib/Entry.svelte
+++ Online_checklist/src/lib/Entry.svelte
@@ -1,20 +1,40 @@
 <script>
     export let title = "Task";
     export let description = "This is the task!";
+    export let task_id = 1; // for determining deletion in json doc
 
 </script>
 
 <header>
-    <h1>{title}</h1>
-    <p>{description}</p>
+    <div>
+        <h1>{title}</h1>
+        <p>{description}</p>
+    </div>
+    <button>X</button>
 </header>
 
 
 <style>
+    button {
+        background-color: red;
+        font-size: 4rem;
+        height: 100%;
+        width: 10%;
+        margin-right: 10px;
+    }
+
+    div {
+       display: flex;
+       flex-direction: column;
+       justify-content: center;
+       align-items: center;
+       width: 90%;
+    }
+
     header {
+        padding: 10px 0 15px 0; 
         display: flex;
-        flex-direction: column;
-        padding: 10px 0 15px 0;
+        flex-direction: row;
         margin: 1% 2% 1% 2%;
         width: 96%;
         height: fit-content;
---
+++ Online_checklist/src/lib/Load.js
@@ -0,0 +1,2 @@
+// attempt to open and read the json file, if someone is using it the fail and show an error message
+
