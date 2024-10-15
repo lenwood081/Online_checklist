99e205d3e4b160a5d0421a4761abf3f3890e36de
lenni migios <lenwood081@gmail.com>
2024-10-15 17:49:42 +1100

    made basjc project

---
+++ Online_checklist/src/lib/Entry.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let title = "Task";
+    export let description = "This is the task!";
+
+</script>
+
+<header>
+    <h1>{title}</h1>
+    <p>{description}</p>
+</header>
+
+
+<style>
+    header {
+        display: flex;
+        flex-direction: column;
+        padding: 10px 0 15px 0;
+        margin: 1% 2% 1% 2%;
+        width: 96%;
+        height: fit-content;
+        align-items: center;
+        border: 1px solid azure;
+    }
+
+    p {
+        font-size: 1.2rem;
+    }
+
+    h1 {
+        font-size: 2rem;
+        padding: 3px;
+    }
+</style>
---
+++ Online_checklist/src/routes/Main/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Entry from "$lib/Entry.svelte";
+</script>
+
+<main>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+    <Entry></Entry>
+</main>
+
+<style>
+    main {
+        display: flex;
+        width: 100%;
+        flex-direction: column;
+        min-height: 100vh;
+        background-color: rgb(40, 38, 38);
+    }
+</style>
